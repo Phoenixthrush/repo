d608e0424b71cd932e0eecbca6182c42d2aa3b9a
フェニックスダラチュ <mr.cetto@phoenixthrush.com>
2022-05-31 12:46:40 -0700

    latest phoenixsploit is broken

---
+++ README.md
@@ -0,0 +1,37 @@
+## Add my APT repo - phoenixsploit package broken
+
+### Installation (debian based distros):
+
+#### Stable
+
+```shell
+curl -sSL phoenixthrush.com/repo/install.sh | bash
+```
+
+#### Use that command to add my repo on WSL (Windows)!
+```shell
+sudo apt update && sudo apt upgrade -y && sudo apt install apt-transport-https curl -y && curl -sSL https://phoenixthrush.com/repo/install.sh | bash
+```
+<br/>
+
+#### Packages:
+
+- phoenixrelay (script which routes all your traffic through a tor relay) <br/>
+```shell
+sudo apt install phoenixrelay -y
+```
+
+- phoenixsploit (idk what it is rn) <br/>
+```shell
+sudo apt install phoenixsploit -y
+```
+
+- hentai (prints an ascii cat) <br/>
+```shell
+sudo apt install hentai -y
+```
+
+- rockyou (downloads the rockyou.txt wordlist) <br/>
+```shell
+sudo apt install rockyou -y
+```
---
+++ install.sh
@@ -0,0 +1,14 @@
+clear
+echo -e "\033[31m[*]\033[00m \033[96mStarting the repo installation!\033[00m"
+sudo echo "idk something that prompts for admin that it looks nice lmao" >/dev/null 2>&1
+echo -e "\n\033[31m[*]\033[00m \033[96mUpdating sources!\033[00m"
+sudo apt update >/dev/null 2>&1
+echo -e "\033[31m[*]\033[00m \033[96mInstalling needed dependencies!\033[00m"
+sudo apt install curl gpg -y >/dev/null 2>&1
+echo -e "\n\033[31m[*]\033[00m \033[96mAdding my repo to your apt source list!\033[00m"
+sudo curl -sSL --compressed -o /etc/apt/sources.list.d/phoenixthrush-packages.list "https://phoenixthrush.com/repo/stable/phoenixthrush-packages.list" >/dev/null 2>&1
+echo -e "\033[31m[*]\033[00m \033[96mAdding new keyring!\033[00m"
+sudo curl -sSL https://phoenixthrush.com/repo/stable/ultrasecretcert.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/phoenixthrush-archive-keyring.gpg >/dev/null 2>&1
+echo -e "\n\033[31m[*]\033[00m \033[96mUpdating sources again!\033[00m"
+sudo apt update >/dev/null 2>&1
+echo -e "\n\033[31m[*]\033[00m \033[96mFinished! \033[00m\033[31m^^\033[00m\n"
